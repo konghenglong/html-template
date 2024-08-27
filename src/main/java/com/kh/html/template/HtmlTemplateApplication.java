a6dac7ae9defd95c18161076d9bc633d28d42901
kongheng-sbilh <kongheng.long@sbilhbank.com.kh>
2024-08-27 09:03:30 +0700

    feat: initial commit

---
+++ src/main/java/com/kh/html/template/HtmlTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.kh.html.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/html/template/HtmlTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.html.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
